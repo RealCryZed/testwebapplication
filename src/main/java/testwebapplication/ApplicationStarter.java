689947b32d2a76f9dec6795db07f00a8416ee6f2
RealCryZed <bujhmgtnhjd@bk.ru>
2022-05-13 16:45:08 +0600

    First commit

---
+++ src/main/java/testwebapplication/ApplicationStarter.java
@@ -0,0 +1,25 @@
+package testwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCacheable(false); //make cacheable in future
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+}
