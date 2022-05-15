29325706ec209c82e9977b6225333655ecca1313
RealCryZed <bujhmgtnhjd@bk.ru>
2022-05-15 12:59:30 +0600

    Fixed bug with figuring out similar words

--- src/main/java/testwebapplication/FirstTaskCore.java
+++ src/main/java/testwebapplication/FirstTaskCore.java
@@ -16,7 +16,7 @@ public class FirstTaskCore {
 
         for (int i = 0; i < array1.length; i++) {
             array1[i] = array1[i].replaceAll("[^\\w]", "");
-            array2[i] = array1[i].replaceAll("[^\\w]", "");
+            array2[i] = array2[i].replaceAll("[^\\w]", "");
         }
 
         for (int i = 0; i < array1.length; i++) {
