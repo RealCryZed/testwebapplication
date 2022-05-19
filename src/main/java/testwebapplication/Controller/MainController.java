646638b18115632a5f2b85b72b52aa71ba0b0015
RealCryZed <bujhmgtnhjd@bk.ru>
2022-05-19 15:21:25 +0600

    Code cleaning

--- src/main/java/testwebapplication/Controller/MainController.java
+++ src/main/java/testwebapplication/Controller/MainController.java
@@ -1,26 +1,9 @@
 package testwebapplication.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import testwebapplication.Functions.FileService;
-import testwebapplication.Functions.FirstTaskCore;
-import testwebapplication.Model.Task1;
-
 import javax.servlet.http.HttpServletRequest;
-import java.io.*;
-import java.net.MalformedURLException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 
 @Controller
 public class MainController {
--- src/main/java/testwebapplication/Functions/SecondTaskCore.java
+++ src/main/java/testwebapplication/Functions/SecondTaskCore.java
@@ -10,40 +10,31 @@ public class SecondTaskCore {
 
         int[][] magicSquare = new int[n][n];
 
-        // Initialize position for 1
         int i = n / 2;
         int j = n - 1;
 
-        // One by one put all values in magic square
         for (int num = 1; num <= n * n;) {
-            if (i == -1 && j == n) // 3rd condition
+            if (i == -1 && j == n)
             {
                 j = n - 2;
                 i = 0;
             }
             else {
-                // 1st condition helper if next number
-                // goes to out of square's right side
                 if (j == n)
                     j = 0;
 
-                // 1st condition helper if next number is
-                // goes to out of square's upper side
                 if (i < 0)
                     i = n - 1;
             }
 
-            // 2nd condition
             if (magicSquare[i][j] != 0) {
                 j -= 2;
                 i++;
                 continue;
             }
             else
-                // set number
                 magicSquare[i][j] = num++;
 
-            // 1st condition
             j++;
             i--;
         }
